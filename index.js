3bcad782f1bd1f6a569560ce0b72bb84bb9f258a
manishamuduli18 <mmuduli927@gmail.com>
2024-07-24 11:03:25 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// var globalvar = "totalAmount";
+// document.addEventListener('DOMContentLoaded', function () {
+//   let totalAmount = 0;
+//   const totalAmountDisplay = document.getElementById('total-amount');
+
+//   const addToCartButtons = document.querySelectorAll('.add-to-cart');
+//   addToCartButtons.forEach(button => {
+//     button.addEventListener('click', function (event) {
+//       event.preventDefault();
+//       const price = parseInt(this.getAttribute('data-price'));
+//       totalAmount += price;
+//       totalAmountDisplay.textContent = totalAmount;
+//     });
+//   });
+// });
+
+const myfunction = () => {
+  // const totalAmount = localStorage.getItem('cartTotal');
+  // const totalAmountDisplay = document.getElementById('total-amount');
+  // if (totalAmount) {
+  //     totalAmountDisplay.textContent = totalAmount;
+  // }
+  document.getElementById("demo1").innerHTML = 123;
+} 
